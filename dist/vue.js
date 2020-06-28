065ac0ef4ae95c0093045f3a2c2277aee3ef3415
kallsave <415034609@qq.com>
2020-06-28 18:33:08 +0800

    init: rollup init

---
+++ dist/vue.js
@@ -0,0 +1,18 @@
+/*!
+ * Vue.js v1.0.0
+ * (c) 2014-2020 Evan You
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global = global || self, global.Vue = factory());
+}(this, (function () { 'use strict';
+
+	var entryRuntimeWithCompiler = {
+
+	};
+
+	return entryRuntimeWithCompiler;
+
+})));
---
+++ scripts/config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const buble = require('rollup-plugin-buble')
+const alias = require('rollup-plugin-alias')
+const version = process.env.VERSION || require('../package.json').version
+
+const banner =
+  '/*!\n' +
+  ` * Vue.js v${version}\n` +
+  ` * (c) 2014-${new Date().getFullYear()} Evan You\n` +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+const aliases = require('./alias')
+const resolve = p => {
+  const base = p.split('/')[0]
+  if (aliases[base]) {
+    return path.resolve(aliases[base], p.slice(base.length + 1))
+  } else {
+    return path.resolve(__dirname, '../', p)
+  }
+}
+
+const builds = {
+  // Runtime+compiler development build (Browser)
+  'web-full-dev': {
+    entry: resolve('web/entry-runtime-with-compiler.js'),
+    dest: resolve('dist/vue.js'),
+    format: 'umd',
+    env: 'development',
+    banner
+  },
+}
+
+function genConfig(name) {
+  const opts = builds[name]
+  const config = {
+    input: opts.entry,
+    external: opts.external,
+    plugins: [
+      alias(Object.assign({}, aliases, opts.alias))
+    ].concat(opts.plugins || []),
+    output: {
+      file: opts.dest,
+      format: opts.format,
+      banner: opts.banner,
+      name: opts.moduleName || 'Vue'
+    },
+  }
+  if (opts.transpile !== false) {
+    config.plugins.push(buble())
+  }
+  return config
+}
+
+if (process.env.TARGET) {
+  module.exports = genConfig(process.env.TARGET)
+} else {
+  exports.getBuild = genConfig
+  exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+}
+
